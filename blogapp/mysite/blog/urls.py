6b30e422284fee08252c71a3d53577c5e56a6528
AQuaZar <greenskullcompany@gmail.com>
2020-02-13 19:48:42 +0200

    Add views and urls for blog

---
+++ blogapp/mysite/blog/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+app_name = "blog"
+
+urlpatterns = [
+    url("", views.post_list, name="post_list"),
+    url(
+        "<int:year>/<int:month>/<int:day>/<slug:post>/",
+        views.post_detail,
+        name="post_detail",
+    ),
+]
