b935a7972dcfd510d9d4d58fb811902c63c20f68
AQuaZar <greenskullcompany@gmail.com>
2020-02-28 17:17:44 +0200

    Add messages on account edit view

--- bookmarks/bookmarks/account/views.py
+++ bookmarks/bookmarks/account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
@@ -63,6 +64,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Profile updated successfully")
+        else:
+            messages.error(request, "Error updating your profile")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
@@ -73,5 +77,3 @@ def edit(request):
         {"user_form": user_form, "profile_form": profile_form},
     )
 
-
-# Create your views here.
